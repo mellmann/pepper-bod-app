95ea2b895a3b83bf7cf94a49d256118b34f8abd3
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2023-09-25 11:56:12 +0200

    cleanup the goto routine corrected the use of futures

--- app/src/main/java/com/storm/pepper/GoToExecutor.java
+++ app/src/main/java/com/storm/pepper/GoToExecutor.java
@@ -14,6 +14,7 @@ import com.aldebaran.qi.sdk.object.actuation.FreeFrame;
 import com.aldebaran.qi.sdk.object.actuation.GoTo;
 import com.aldebaran.qi.sdk.object.actuation.Mapping;
 import com.aldebaran.qi.sdk.object.geometry.Transform;
+import com.aldebaran.qi.sdk.object.geometry.Vector3;
 import com.aldebaran.qi.sdk.util.FutureUtils;
 
 import java.util.ArrayList;
@@ -25,51 +26,88 @@ public class GoToExecutor {
 
     private static final String TAG = GoToExecutor.class.getSimpleName();
 
-    // public void setValues()
-    double[] xPositions = { -0.5, 0.0, 0.5 };
-    double[] yPositions = { -0.5, 0,0, 0.5 };
-
     private ArrayList<FreeFrame> targetPos = new ArrayList<>();
+
+    private int targetLocationIndex = 0;
+
+    // this is used by the sense arrived_at_location
+    private boolean arrivedAtLocation = false;
+    private long timeWhenArivedAtLocation = 0;
+    private int currentLocationIndex = 0;
+
     private Actuation actuation;
     private Mapping mapping;
 
-    private int index = 0;
-
     private void initTargetPositions(PepperLog pepperLog) {
-        targetPos.add(makeRelativeFreeFrame(1.0, 0.0, pepperLog));
-        targetPos.add(makeRelativeFreeFrame(0.0, 0.0, pepperLog));
+        targetPos.add(makeRelativeFreeFrame(1.0,                 0.0, pepperLog));
+        targetPos.add(makeRelativeFreeFrame(0.5,  Math.sqrt(3.0)/2.0, pepperLog));
+        targetPos.add(makeRelativeFreeFrame(0.0,                 0.0, pepperLog));
     }
 
-    public void resetTargetPositions() {
+    public void reset()
+    {
+        stopMoving();
         targetPos.clear();
+
+        arrivedAtLocation = false;
+        targetLocationIndex = 0;
+        currentLocationIndex = 0;
     }
 
-    private FreeFrame makeRelativeFreeFrame(double x, double y, PepperLog pepperLog) {
-        Frame baseFrame = actuation.robotFrame();
-        FreeFrame locationFrame = mapping.makeFreeFrame();
-
-        Transform transform = TransformBuilder.create().from2DTranslation(x, y);
-        locationFrame.update(baseFrame, transform, 0L);
-        return locationFrame;
+    public void stopMoving() {
+        if(gotoFuture != null) {
+            gotoFuture.cancel(true);
+        }
     }
+
+
+    private FreeFrame makeRelativeFreeFrame(double x, double y, PepperLog pepperLog)
+    {
+        return actuation.async().robotFrame().andThenApply(robotFrame -> {
+            // Create a FreeFrame representing the current robot frame.
+            FreeFrame locationFrame = mapping.makeFreeFrame();
+            //Transform transform = TransformBuilder.create().from2DTranslation(x, y);
+            Transform transform = TransformBuilder.create().fromTranslation(new Vector3(x, y, 1.80));
+            locationFrame.update(robotFrame, transform, 0L);
+
+            // Store the FreeFrame in the next slot.
+            return locationFrame;
+        }).getValue();
+    }
+
+    public int getCurrentLocationIndex() {
+        return currentLocationIndex;
+    }
+
+    public boolean getArrivedAtLocation() {
+        return arrivedAtLocation;
+    }
+
+    public double getTimeSinceArrived() {
+        return (double)(System.currentTimeMillis() - timeWhenArivedAtLocation) / 1000.0;
+    }
+
+    private Future<Void> gotoFuture = null;
 
     public void performGotoSquare(QiContext qiContext, Actuation actuation, Mapping mapping,
                             POSHBehaviourLibrary pbl, PepperLog pepperLog) {
 
-        FutureUtils.wait(0, TimeUnit.SECONDS).andThenConsume((ignore) -> {
+        pbl.setAnimating(true);
+        this.actuation = actuation;
+        this.mapping = mapping;
 
-            pbl.setAnimating(true);
-            this.actuation = actuation;
-            this.mapping = mapping;
+        if(targetPos.isEmpty()) {
+            initTargetPositions(pepperLog);
+            pepperLog.appendLog(TAG, "Target Frame initialized");
+        }
 
-            if(targetPos.isEmpty()) {
-                initTargetPositions(pepperLog);
-                //pepperLog.appendLog(TAG, "Target Frame initialized");
-            }
+        // select next point to go to
+        targetLocationIndex = (targetLocationIndex + 1) % targetPos.size();
+        FreeFrame locationFrame = targetPos.get(targetLocationIndex);
+        pepperLog.appendLog(TAG, "FreeFrame initialized " + targetLocationIndex);
 
-            index = (index + 1) % targetPos.size();
-            FreeFrame locationFrame = targetPos.get(index);
-            //pepperLog.appendLog(TAG, "FreeFrame initialisiert " + index);
+        Future<Frame> frameFuture = locationFrame.async().frame();
+        frameFuture.andThenCompose(frame -> {
 
             // Create a GoTo action.
             GoTo goTo = GoToBuilder.with(qiContext)
@@ -79,22 +117,33 @@ public class GoToExecutor {
             // pepperLog.appendLog(TAG, "FreeFrame locationFrame");
 
             // Display text when the GoTo action starts.
-            goTo.addOnStartedListener(() -> pepperLog.appendLog(TAG, "Roaming started"));
+            goTo.addOnStartedListener(() -> {
+                arrivedAtLocation = false;
+                pepperLog.appendLog(TAG, "Roaming started");
+            });
 
             // Execute the GoTo action asynchronously.
-            Future<Void> goToFuture = goTo.async().run();
+            gotoFuture = goTo.async().run();
 
-            goToFuture.thenConsume(future -> {
-                if (future.isSuccess()) {
-                    pepperLog.appendLog(TAG, "Goto finished with success!");
-                } else if (future.hasError()) {
-                    pepperLog.appendLog(TAG, "Goto has error!");
-                } else if (future.isCancelled()) {
-                    pepperLog.appendLog(TAG, "Goto has been cancelled, will turn around!");
-                }
-                pbl.setAnimating(false);
-                pbl.setReachedPosition(true);
-            });
+            return gotoFuture;
+        }).thenConsume(future -> {
+            if (future.isSuccess()) {
+                pepperLog.appendLog(TAG, "Goto finished with success!");
+
+                arrivedAtLocation = true;
+                timeWhenArivedAtLocation = System.currentTimeMillis();
+
+                currentLocationIndex = targetLocationIndex;
+                pepperLog.appendLog(TAG, "Arrived at location: " + currentLocationIndex);
+            } else if (future.hasError()) {
+                pepperLog.appendLog(TAG, "Goto has error!");
+            } else if (future.isCancelled()) {
+                pepperLog.appendLog(TAG, "Goto has been cancelled, will stop here!");
+            }
+
+            gotoFuture = null;
+            pbl.setAnimating(false);
+            pbl.setReachedPosition(true);
         });
     }
 
